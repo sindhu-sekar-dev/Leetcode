e102885d3b21b081ba40a2ce9fc1ada1d6bb62b9
sindhusekar <sindhusekar711@gmail.com>
2026-08-30 23:35:14 +0530

    Time: 0 ms (100%), Space: 46.4 MB (48.72%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -2,7 +2,6 @@ class Solution {
     public int maxProfit(int[] prices) {
         int profit = 0;
 
-        // Sum all positive adjacent differences.
         for (int i = 1; i < prices.length; i++) {
             if (prices[i] > prices[i - 1]) {
                 profit += prices[i] - prices[i - 1];
